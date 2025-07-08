bc54c65114de107db09f014dd88414d03eeb46ad
Ashraf Ainia <a.ainia.c@nhc.sa>
2025-07-08 11:59:11 +0300

    feat: add Button component with customizable styles and stories

---
+++ src/v2/components/Button/index.stories.tsx
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Button, { sizes } from ".";
+
+const meta = {
+  title: "DGAUI/V2/Button",
+  component: Button,
+  parameters: { layout: "centered" },
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      options: Object.keys(sizes),
+      control: { type: "select" },
+    },
+    disabled: {
+      options: [false, true],
+      control: { type: "boolean" },
+    },
+  },
+  args: {
+    disabled: false,
+    destructive: false,
+    iconOnly: false,
+    onColor: false,
+    style: "primary",
+    size: "large",
+    leadIcon: "+",
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+// Stories
+type Story = StoryObj<typeof Button>;
+
+export const Default = (args: any) => {
+  if (args.onColor) {
+    return (
+      <div
+        style={{
+          backgroundColor: "#074D31",
+          padding: 56,
+          minWidth: 400,
+          textAlign: "center",
+        }}
+      >
+        <Button {...args}>Button</Button>
+      </div>
+    );
+  }
+  return <Button {...args}>Button</Button>;
+};
