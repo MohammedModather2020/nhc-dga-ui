3ecabed4768bb52f93fab90d1c3f3c14d6eb1804
Ashraf Ainia <a.ainia.c@nhc.sa>
2025-07-08 11:28:40 +0300

    feat: Button version 2

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,15 @@
-export const variants: Variant[] = ["contained", "outlined", "text", "transparent", "solid"];
+export const variants: Variant[] = [
+  "contained",
+  "outlined",
+  "text",
+  "transparent",
+  "solid",
+];
+
+export const COLORS = {
+  white: "#FFF",
+  white20: "#FFF3",
+  white40: "#FFF6",
+  white60: "#FFF9",
+  white80: "#FFFC",
+};
