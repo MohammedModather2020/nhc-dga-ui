88b53036eaa1d9e5b3b069fc45a97eeab9d93a8a
MohammedModather2020 <mohammedmodather144@gmail.com>
2025-07-14 09:04:15 +0300

    feat: add Notification component with customizable styles and icons v2

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -1,5 +1,6 @@
 import Button from "./components/Button";
 import Loading from "./components/Loading";
 import Checkbox from "./components/Checkbox";
+import Notification from "./components/Notification";
 
-export { Button, Loading, Checkbox };
+export { Button, Loading, Checkbox, Notification };
