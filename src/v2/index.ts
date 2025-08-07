8a3791167325492026e77eed4de61f4071aa5045
MohammedModather2020 <mohammedmodather144@gmail.com>
2025-08-07 09:37:07 +0300

    refactor: standardize import/export formatting in index.ts

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -1,20 +1,27 @@
-import Button from "./components/Button";
-import Loading from "./components/Loading";
-import Tag from "./components/Tag";
-import InlineAlert from "./components/InlineAlert";
-import StatusTag from "./components/StatusTag";
-import Accordion from "./components/Accordion";
-import Avatar from "./components/Avatar";
-import AvatarGroup from "./components/AvatarGroup";
-import Link from "./components/Link";
-import Checkbox from "./components/Checkbox";
-import NavigationDrawerItem from "./components/NavigationDrawerItem";
-import Pagination from "./components/Pagination";
+import Button from './components/Button';
+import Loading from './components/Loading';
+import Tag from './components/Tag';
+import InlineAlert from './components/InlineAlert';
+import StatusTag from './components/StatusTag';
+import Accordion from './components/Accordion';
+import Avatar from './components/Avatar';
+import AvatarGroup from './components/AvatarGroup';
+import Link from './components/Link';
+import Checkbox from './components/Checkbox';
+import NavigationDrawerItem from './components/NavigationDrawerItem';
+import Pagination from './components/Pagination';
 
-
-
-
-
-export { Button, Loading,  Tag, Accordion, Avatar, AvatarGroup, Link, Checkbox , Pagination, InlineAlert, StatusTag , Notification, NavigationDrawerItem };
-
-
+export {
+	Button,
+	Loading,
+	Tag,
+	Accordion,
+	Avatar,
+	AvatarGroup,
+	Link,
+	Checkbox,
+	Pagination,
+	InlineAlert,
+	StatusTag,
+	NavigationDrawerItem,
+};
