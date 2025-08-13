23d16b2e758b7c94772e6e7ecb039fab85294ba8
Ashraf Ainia <a.ainia.c@nhc.sa>
2025-08-13 18:22:03 +0300

    feat: Implement TextInput v2 component with styles and stories

---
+++ src/v2/components/TextInput/index.stories.tsx
@@ -0,0 +1,85 @@
+import { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import TextInput, { sizes } from ".";
+import withRtl from "../../../lib/RTL";
+import Button from "../Button";
+
+const meta = {
+  title: "DGAUI/V2/TextInput",
+  component: TextInput,
+  parameters: { layout: "centered" },
+  tags: ["autodocs"],
+  argTypes: {
+    label: {
+      control: { type: "text" },
+    },
+    size: {
+      options: Object.keys(sizes),
+      control: { type: "radio" },
+      table: { defaultValue: { summary: "large" } },
+    },
+    error: {
+      control: { type: "boolean" },
+    },
+    disabled: {
+      control: { type: "boolean" },
+    },
+  },
+  args: {
+    size: "large",
+    label: "Label",
+    style: "default",
+    error: false,
+    readOnly: false,
+    disabled: false,
+    prefix: "#",
+    suffix: "",
+  },
+} satisfies Meta<typeof TextInput>;
+
+export default meta;
+
+// Stories
+type Story = StoryObj<typeof TextInput>;
+
+export const Default: Story = {
+  args: {
+    label: "Label",
+    placeholder: "Placeholder text",
+    id: "text-input1",
+  },
+};
+
+export const Prefix: Story = {
+  args: {
+    label: "Label",
+    placeholder: "Enter price",
+    type: "number",
+    prefix: "$",
+  },
+};
+
+export const Error: Story = {
+  args: {
+    label: "Label",
+    placeholder: "Placeholder text",
+    error: true,
+  },
+};
+
+export const RTL = withRtl(() => (
+  <TextInput label="العنوان" placeholder="نص توضيحي" icon="🔎"/>
+));
+
+export const Ref = () => {
+  const ref = React.useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <Button onClick={() => alert(ref.current?.value)}>
+        Alert TextInput value by ref
+      </Button>
+      <TextInput ref={ref} label="Label" placeholder="placeholder" />
+    </>
+  );
+};
